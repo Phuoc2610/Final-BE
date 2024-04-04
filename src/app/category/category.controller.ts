0db5705410e6ede762c2d96f82fb51120377f6d3
vanphuoc <phuoc0905886611@gmail.com>
2024-04-04 21:29:52 +0700

    create category

--- src/app/category/category.controller.ts
+++ src/app/category/category.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { CategoryService } from "./category.service";
-import { CreateCategoryDTO } from "./dto.ts/create.category.dto";
+import { CreateCategoryDTO } from "./dto/create.category.dto";
+import { UpdateCategoryDTO } from "./dto/update.category.dto";
 
 @Controller('category')
 @ApiTags('Category')
@@ -16,9 +17,25 @@ export class CategoryController{
     getAll(){
         return this.categoryService.findAll()
     }
+
+    @Get(':id')
+    getById(@Param('id') id:string) {
+        return this.categoryService.findById(id)
+    }
+
+    @Patch(':id')
+    updateCategory(@Param('id') id:string , @Body() updateCategoryDTO : UpdateCategoryDTO){
+        return this.categoryService.update(id, updateCategoryDTO);
+    }
+    
     @Post('')
     createCategory(@Body() createCategoryDTO: CreateCategoryDTO){
         return this.categoryService.create(createCategoryDTO);
+    }
+
+    @Delete(':id')
+    deleteCategory(@Param('id') id:string){
+        return this.deleteCategory(id)
     }
     
 }
--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { Category } from "@prisma/client";
 import { PrismaService } from "src/database/prisma/prisma.service";
-import { CreateCategoryDTO } from "./dto.ts/create.category.dto";
+import { CreateCategoryDTO } from "./dto/create.category.dto";
+import { UpdateCategoryDTO } from "./dto/update.category.dto";
 
 @Injectable({})
 export class CategoryService {
@@ -11,14 +12,54 @@ export class CategoryService {
     ) {
     }
 
-    async findAll():Promise<Category[]>{
+    async findAll(): Promise<Category[]> {
         return await this.prismaService.category.findMany();
     }
-    async create(createCategoryDTO: CreateCategoryDTO) : Promise<Category>{
-        return  await this.prismaService.category.create({
-            data:{
+
+    async findById(id: string): Promise<Category> {
+        if (!id) {
+            throw new ForbiddenException("Error Id:")
+        }
+        const category = await this.prismaService.category.findUnique({
+            where: {
+                id: id
+            },
+
+        })
+        if (!category) {
+            throw new ForbiddenException("Error category")
+        }
+        return category
+    }
+    async create(createCategoryDTO: CreateCategoryDTO): Promise<Category> {
+        return await this.prismaService.category.create({
+            data: {
                 ...createCategoryDTO
             }
         })
-     }
+    }
+
+    async update(id: string, updateCatgoryDTO: UpdateCategoryDTO): Promise<Category> {
+        const category = this.findById(id)
+        if (!category) {
+            throw new ForbiddenException("Error id")
+        }
+        return await this.prismaService.category.update({
+            where: {
+                id: id
+            },
+            data: {
+                ...updateCatgoryDTO
+            }
+        })
+    }
+    async deleteCategory(id: string): Promise<any> {
+        const category = await this.findById(id);
+        // if(category.)
+        return await this.prismaService.category.delete({
+            where: {
+                id: id
+            }
+        })
+    }
 }
