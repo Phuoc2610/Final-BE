d24ff68898bcdfa2844c801d5289de4421f3c586
vanphuoc <phuoc0905886611@gmail.com>
2024-04-21 04:11:25 +0700

    authorize

---
+++ prisma/data/roles.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.roles = void 0;
+exports.roles = [
+    {
+        id: 'clgywnc7h000008l33v1ja5g9',
+        name: 'Admin'
+    },
+    {
+        id: 'clgywqlp0000408l38j7zarnn',
+        name: 'Store'
+    },
+    {
+        id: 'clgywq0h8000308l3a38y39t6',
+        name: 'User'
+    }
+];
